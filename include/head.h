1487705d0e49f15c5f2ed9c9d78ba7ba3c6ad174
rewixx <rewixx644@gmail.com>
2023-10-07 22:56:03 +0200

    added template with my vec2 lib

---
+++ include/head.h
@@ -0,0 +1,20 @@
+//c++ stuff
+#include<iostream>
+//#include<string>
+//sdl stuff
+#include<SDL2/SDL.h>
+
+//width and heigth of the window
+const int WIDTH=720;
+const int HEIGTH=128;
+
+//pixel size
+//TODO:diffrent pixel sizes 
+const int PIXEL_SIZE=5;
+
+//need to use #define const is doing something funny
+//TODO:not the name uman sus or i will be kiled in a fire.
+#define WINDOW_NAME "uman sus"
+
+
+
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "include/head.h"
+//TODO: MAKE main LOOK BETTER
+int main(int argc, char* argv[])
+{
+    //making the window 
+    //dose the init work??
+    if(SDL_Init(SDL_INIT_EVERYTHING)<0){
+	    std::cout<< "SDL is " <<SDL_GetError() <<"\n";
+    }
+
+    SDL_Window* window=nullptr;
+    SDL_Renderer* renderer=nullptr;
+    bool is_running=true;
+    //defines the window 
+    window = SDL_CreateWindow("SDL Templete",
+		    0,
+		    0,
+		    WIDTH*PIXEL_SIZE,
+		    HEIGTH*PIXEL_SIZE,
+		    SDL_WINDOW_MAXIMIZED);
+
+    renderer =SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+   
+    SDL_RenderSetScale(renderer,PIXEL_SIZE,PIXEL_SIZE);
+
+    // the GAMELOOP :
+    while(is_running){
+    SDL_Event event;	
+
+     while(SDL_PollEvent(&event)){
+
+	     if(event.type == SDL_QUIT){
+
+		      is_running=false;
+	     };
+	  };
+
+	  SDL_RenderPresent(renderer);
+
+    };
+    
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+};
